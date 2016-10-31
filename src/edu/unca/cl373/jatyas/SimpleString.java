9b5c5330ebafd7b444fee25c7456411b96c6285d
Joel Atyas <jatyas@gmail.com>
2016-10-31 14:02:38 -0400

    Initial Commit of SimpleString project
    -Only stubs of methods

---
+++ src/edu/unca/cl373/jatyas/SimpleString.java
@@ -0,0 +1,79 @@
+package edu.unca.cl373.jatyas;
+
+import java.util.*;
+
+public class SimpleString {
+	
+	static final String NOT_IMPLEMENTED_ERROR = "This function is not yet implemented.";
+
+	List<Character> charContents;
+	
+	public SimpleString(char[] letters) {
+		
+	}
+
+	private List<Character> toCharacterList(char[] letters){
+		throw new UnsupportedOperationException(NOT_IMPLEMENTED_ERROR);
+	}
+	
+	public char charAt(int index){
+		throw new UnsupportedOperationException(NOT_IMPLEMENTED_ERROR);
+	}
+	
+	public boolean contains(char c){
+		throw new UnsupportedOperationException(NOT_IMPLEMENTED_ERROR);
+	}
+	
+	public int indexOf(char c){
+		throw new UnsupportedOperationException(NOT_IMPLEMENTED_ERROR);
+	}
+	
+	public boolean isEmpty(){
+		throw new UnsupportedOperationException(NOT_IMPLEMENTED_ERROR);
+	}
+	
+	public int length(){
+		throw new UnsupportedOperationException(NOT_IMPLEMENTED_ERROR);
+	}
+	
+	public SimpleString concat(SimpleString s){
+		throw new UnsupportedOperationException(NOT_IMPLEMENTED_ERROR);
+	}
+	
+	public SimpleString subString(int startIndex, int endIndex){
+		throw new UnsupportedOperationException(NOT_IMPLEMENTED_ERROR);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((charContents == null) ? 0 : charContents.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof SimpleString)) {
+			return false;
+		}
+		SimpleString other = (SimpleString) obj;
+		if (charContents == null) {
+			if (other.charContents != null) {
+				return false;
+			}
+		} else if (!charContents.equals(other.charContents)) {
+			return false;
+		}
+		return true;
+	}
+	
+	
+}
+	
---
+++ src/edu/unca/cl373/jatyas/TestBed.java
@@ -0,0 +1,14 @@
+package edu.unca.cl373.jatyas;
+
+public class TestBed {
+
+	public TestBed() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
